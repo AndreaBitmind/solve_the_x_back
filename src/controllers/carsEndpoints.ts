797c61fb6550870c01bf589b5cb29e123c545f79
Andrea Martinez <andrea.maort@gmail.com>
2022-11-05 16:07:30 +0100

    changed id type from number to string in cars endpoints

--- src/controllers/carsEndpoints.ts
+++ src/controllers/carsEndpoints.ts
@@ -2,6 +2,6 @@ const knex = require("../database/knex");
 
 export const createCar = (car: string) => knex("cars").insert(car);
 export const getAllCars = () => knex("cars").select("*");
-export const deleteCar = (id: number) => knex("cars").where("id", id).del();
-export const updateCar = (id: number, car: string) =>
+export const deleteCar = (id: string) => knex("cars").where("id", id).del();
+export const updateCar = (id: string, car: string) =>
   knex("cars").where("id", id).update(car);
